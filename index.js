f72e7599a50b8ae8d09f1357171f9b36e9092601
sarath <sarath.sct@gmail.com>
2018-03-21 17:59:50 +0530

    final

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 const yarg = require('yargs');
-const fs=require('fs');
-var options=yarg.argv.options;
+const fs = require('fs');
+var options = yarg.argv.options;
 
-console.log(options);
+// console.log(options);
 
-fs.writeFile('name.json', ' { "name" : [', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });   
-
-if(options=='read'){
-
+var obj = {
+    name: []
 }
-else if(options=='write'){
-    console.log(yarg.argv._[0]);
-    var name=yarg.argv._[0];
-    fs.appendFile('name.json', '"'+name+'",', function (err) {
+var i=1;
+if (options == 'read') {
+    var data = fs.readFileSync('name.json');
+    existdata = JSON.parse(data.toString());
+    console.log('.......Names.........');
+    existdata.name.forEach(item => {
+        console.log(i++ +' '+ item);
+    });
+}
+else if (options == 'write') {
+    // console.log(yarg.argv._[0]);
+    var name = yarg.argv._[0];
+
+
+    var existdata;
+    var data = fs.readFileSync('name.json');
+    existdata = JSON.parse(data.toString());
+    // console.log('exist', existdata.name);
+    // console.log('yoo', existdata);
+
+
+    existdata.name.forEach(item => {
+        obj["name"].push(item);
+    });
+
+    obj["name"].push(name);
+    // console.log(JSON.stringify(obj));
+
+    fs.writeFile('name.json', JSON.stringify(obj), function (err) {
         if (err) throw err;
-        console.log('Saved!');
-      });
+        console.log('Saved ' + name);
+    });
 }
-else if(options=='remove'){
-    
+else if (options == 'remove') {
+    var removename = yarg.argv._[0];
+    var data = fs.readFileSync('name.json');
+    existdata = JSON.parse(data.toString());
+    var index= existdata.name.indexOf(removename)
+    // console.log('index',index);
+    if (index > -1) {
+        existdata.name.splice(index, 1);
+        console.log('Removed.........');
+    }
+
+    console.log('.......Names.........');
+    existdata.name.forEach(item => {
+        console.log(i++ +' '+ item);
+    });
+
+    fs.writeFile('name.json', JSON.stringify(existdata), function (err) {
+        if (err) throw err;
+        // console.log('Saved!');
+    });
+
 }
 
-fs.appendFile('name.json', ']}', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
